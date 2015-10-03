8609f3de2f1830caf06d5fb19243e14ef627e4d8
Asim Zeeshan <asim@techbytes.pk>
2015-10-03 15:17:56 +0500

    Database config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yii2',
+    'username' => 'yii2',
+    'password' => 'Lz2u6EwSzTCbZfyn',
     'charset' => 'utf8',
 ];
