ffb8f60163953702cf42ea079a15993f0148b999
Asim Zeeshan <asim@techbytes.pk>
2015-10-08 01:55:34 +0500

    Fixing default sort for ShipmentSearch

--- models/ShipmentSearch.php
+++ models/ShipmentSearch.php
@@ -45,6 +45,7 @@ class ShipmentSearch extends Shipment
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort'=> ['defaultOrder' => ['id'=>SORT_DESC]]
         ]);
 
         $this->load($params);
