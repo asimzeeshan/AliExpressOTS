c330051205f9f485f58e418d4bb78f1ec50a0f7a
Asim Zeeshan <asim@techbytes.pk>
2015-12-16 18:54:15 +0500

    Fixing a Blunder - wrong logout URL

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -64,7 +64,7 @@ AppAsset::register($this);
                 ['label' => 'Login', 'url' => ['/site/login']] :
                 [
                     'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
+                    'url' => ['/logout'],
                     'linkOptions' => ['data-method' => 'post']
                 ],
         ],
